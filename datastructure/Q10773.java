27d2beae5678c75ebde51c6cbc87be86a6528d00
rosesua318 <rosesua@duksung.ac.kr>
2023-04-30 00:03:47 +0900

    [#92] 백준 10773번 제로 문제 완료

---
+++ datastructure/Q10773.java
@@ -0,0 +1,26 @@
+package datastructure;
+
+import java.util.*;
+
+public class Q10773 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int k = sc.nextInt();
+
+        Stack<Integer> stack = new Stack<>();
+        for(int i = 0; i < k; i++) {
+            int n = sc.nextInt();
+            if(n == 0) {
+                stack.pop();
+            } else {
+                stack.push(n);
+            }
+        }
+
+        int sum = 0;
+        while(!stack.isEmpty()) {
+            sum += stack.pop();
+        }
+        System.out.println(sum);
+    }
+}

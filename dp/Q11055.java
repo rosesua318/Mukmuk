848136840de82bbd2292cb6ea89523a19b2643ad
rosesua318 <rosesua@duksung.ac.kr>
2023-05-16 14:39:57 +0900

    [#179] 백준 11055번 가장 큰 증가하는 부분 수열 문제 완료

---
+++ dp/Q11055.java
@@ -0,0 +1,30 @@
+package dp;
+
+import java.util.*;
+
+public class Q11055 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        for(int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        int d[] = new int[n];
+        for(int i = 0; i < n; i++) {
+            d[i] = a[i];
+            for(int j = 0; j < i; j++) {
+                if(a[j] < a[i] && d[i] < d[j] + a[i]) {
+                    d[i] = d[j] + a[i];
+                }
+            }
+        }
+
+        int answer = d[0];
+        for(int i = 0; i < n; i++) {
+            answer = Math.max(answer, d[i]);
+        }
+        System.out.println(answer);
+    }
+}

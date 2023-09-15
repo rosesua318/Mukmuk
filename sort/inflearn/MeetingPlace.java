301a0c688ef689143eb60cd34a39bc3d06ce6eae
rosesua <rosesua@duksung.ac.kr>
2023-09-16 02:03:15 +0900

    인프런 모임장소, 멀티태스킹 설명 추가

--- sort/inflearn/MeetingPlace.java
+++ sort/inflearn/MeetingPlace.java
@@ -6,8 +6,8 @@ public class MeetingPlace {
     public static int solution(int[][] board) {
         int answer = 0;
         int n = board.length;
-        ArrayList<Integer> row = new ArrayList<>();
-        ArrayList<Integer> col = new ArrayList<>();
+        ArrayList<Integer> row = new ArrayList<>(); // 사람들의 행 위치를 저장하기 위해
+        ArrayList<Integer> col = new ArrayList<>(); // 사람들의 열 위치를 저장하기 위해
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < n; j++) {
                 if(board[i][j] == 1) { // 사람이 위치한 경우
@@ -16,17 +16,17 @@ public class MeetingPlace {
                 }
             }
         }
-        col.sort((a, b) -> a - b); // 열은 오름차순 정렬
-        int x = row.get(row.size() / 2); // 행의 중값 위치값 구하기
-        int y = col.get(col.size() / 2); // 열의 중간 위치값 구하기
-        // (x, y)가 사람들이 만나야할 지점이다.
-        for(int p : row) {
-            answer += Math.abs(x - p); // 각각의 사람들의 행의 위치와 만나야할 지점의 행의 위치 절댓값 구해서 이동거리에 더해주기
+        col.sort((a, b) -> a - b); // 열은 j값들이기 때문에 정렬이 안되어 있는 상태이므로 오름차순 정렬
+        int x = row.get(row.size() / 2); // 학생들이 있는 행 개수의 절반 지점으로 모일 장소의 행 위치 구하기
+        int y = col.get(col.size() / 2); // 학생들이 있는 열 개수의 절반 지점으로 모일 장소의 열 위치 구하기
+        // (x, y)가 모일 장소 지점이다.
+        for(int p : row) { // 사람들의 행 위치 탐색
+            answer += Math.abs(x - p); // 각각의 사람들의 행의 위치와 만나야할 지점의 행의 위치 차이를 절댓값으로 구해서 이동거리에 더해주기
         }
-        for(int p : col) {
-            answer += Math.abs(y - p); // 각각의 사람들의 열의 위치와 만나야할 지점의 열의 위치 절댓값 구해서 이동거리에 더해주기
+        for(int p : col) { // 사람들의 열 위치 탐색
+            answer += Math.abs(y - p); // 각각의 사람들의 열의 위치와 만나야할 지점의 열의 위치 차이를 절댓값으로 구해서 이동거리에 더해주기
         }
-        return answer;
+        return answer; // 이동 거리 합의 최소값 리턴
     }
     public static void main(String[] args) {
         System.out.println(MeetingPlace.solution(new int[][]{{1, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 1}, {0, 0, 0, 0, 0}, {0, 0, 1, 0, 0}}));
--- sort/inflearn/Multitasking.java
+++ sort/inflearn/Multitasking.java
@@ -4,24 +4,24 @@ import java.util.Arrays;
 
 public class Multitasking {
     public static int solution(int[] tasks, long k) {
-        int st[] = new int[tasks.length + 1]; // 0번째에 0을 넣어야 하기 때문
-        System.arraycopy(tasks, 0, st, 1, tasks.length);
-        Arrays.sort(st); // 작업의 소요 시간을 기준으로 오름차순 정렬
+        int st[] = new int[tasks.length + 1]; // tasks와 동일한데 0번째에 0을 넣어야 하기 때문에 길이 +1 해줌
+        System.arraycopy(tasks, 0, st, 1, tasks.length); // tasks 내용을 전체를 st의 1번째부터 복사
+        Arrays.sort(st); // 작업의 완료 시간을 기준으로 오름차순 정렬
         int rest = tasks.length; // 남은 작업의 개수
-        for(int i = 1; i < st.length; i++) {
-            long time = ((long) rest * (st[i] - st[i - 1])); // 해당 작업을 끝내는데 필요한 시간(회전 고려)
-            if(k < time) {
-                long index = k % rest; // 남아있는 작업들 중 k초 후 시작처리해야 작업이 몇번째인지 (% rest한 까닭은 무의미한 회전을 하지 않도록)
+        for(int i = 1; i < st.length; i++) { // 가장빨리 끝나는 작업부터 오래 걸리는 작업까지 탐색하기
+            long time = ((long) rest * (st[i] - st[i - 1])); // 해당 작업을 끝내는데 필요한 시간(회전 고려) - 남은 작업의 수 * 남은 회전 횟수
+            if(k < time) { // 정전 전까지 해당 작업을 끝내지 못하는 경우
+                long index = k % rest; // 남아있는 작업들 중 k초 후 시작처리해야 작업이 몇번째인지 구하기(% rest한 까닭은 회전을 복잡하게 구현하지 않고 회전하고 남은 초만 고려하게)
                 int count = 0;
                 for(int j = 0; j < tasks.length; j++) {
-                    if(tasks[j] >= st[i]) { // 이미 끝난 작업은 제외시키기 위함
-                        if(count == index) { // 정전되고 난 후에 처음으로 처리해야 할 인덱스
-                            return j + 1; // 인덱스에서 1을 더한 값을 리턴해야 함
+                    if(tasks[j] >= st[i]) { // 현재 작업이 걸리는 시간 보다 큰 작업인 경우 : 정전 후에도 끝나지 않는 경우
+                        if(count == index) { // 정전되고 난 후에 처음으로 처리해야 할 작업의 인덱스인 경우
+                            return j + 1; // 인덱스에서 1을 더한 값을 리턴해야 작업의 인덱스임
                         }
-                        count++;
+                        count++; // 카운트 증가
                     }
                 }
-            } else {
+            } else { // 정전 전까지 해당 작업을 끝내는 경우
                 k -= time; // k초에서 time 빼주기 (남은 시간 갱신)
                 rest--; // 하나의 작업이 끝났으므로 남은 작업의 개수 1 감소
             }

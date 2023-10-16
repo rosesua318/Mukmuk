d8ab5145582bce89ae0b0ee415b204f6c99b9c17
rosesua <rosesua@duksung.ac.kr>
2023-10-16 19:43:08 +0900

    백준 기초 1/2 다이나믹 프로그래밍 1, 2, 3 더하기 문제까지 코드 설명 추가

--- dp/Q11727.java
+++ dp/Q11727.java
@@ -6,14 +6,18 @@ public class Q11727 {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int d[] = new int[1001];
-        d[0] = 1;
-        d[1] = 1;
+        int d[] = new int[1001]; // d[i] : 2xi 직사각형을 채우는 방법의 수
+        d[0] = 1; // 0도 1로 초기화 해주기
+        d[1] = 1; // 2x1 직사각형을 채우는 방법의 수는 1개
 
-        for(int i = 2; i <= n; i++) {
+        for(int i = 2; i <= n; i++) { // 2부터 n까지
+            // 1. 맨 오른쪽에 세로 사각형 1개를 놓는 경우 : 세로 사각형 제외하면 크기가 2x(i-1)이 되고 이는 d[i-1]임.
+            // 2. 맨 오른쪽에 가로 사각형 2개를 놓는 경우 : 가로 사각형 2개를 제외하면 크기가 2x(i-2)가 되고 이는 d[i-2]임.
+            // 3. 맨 오른쪽에 2x2 사각형 1개를 놓는 경우 : 2x2 사각형 제외하면 크기가 2x(i-2)가 되고 이는 d[i-2]임.
+            // -> d[i]는 이 세 가지 방법의 수의 합이므로 d[i] = d[i - 1] + d[i - 2] * 2가 됨.
             d[i] = d[i - 1] + d[i - 2] * 2;
             d[i] %= 10007;
         }
-        System.out.println(d[n]);
+        System.out.println(d[n]); // 2xn 직사각형을 채우는 방법의 수 출력
     }
 }
--- dp/Q1463.java
+++ dp/Q1463.java
@@ -2,21 +2,22 @@ package dp;
 
 import java.util.*;
 
+// 백준 1463번 1로 만들기
 public class Q1463 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int x = sc.nextInt();
-        int d[] = new int[x + 1];
-        d[1] = 0;
-        for(int i = 2; i <= x; i++) {
-            d[i] = d[i - 1] + 1;
-            if(i % 2 == 0) {
-                d[i] = Math.min(d[i], d[i / 2] + 1);
+        int x = sc.nextInt(); // 정수 x 입력 받기
+        int d[] = new int[x + 1]; // d[i] : i를 1로 만드는 데 필요한 최소 연산 횟수
+        d[1] = 0; // 1을 1로 만드는 데 필요한 연산 횟수는 0
+        for(int i = 2; i <= x; i++) { // 2부터 x까지 반복
+            d[i] = d[i - 1] + 1; // 1을 빼는 연산. i->i-1로 만들기 위해 연산 1번 + i-1->1로 만들기 위해 필요한 연산 d[i-1]
+            if(i % 2 == 0) { // 2로 나누어 떨어지는 경우
+                d[i] = Math.min(d[i], d[i / 2] + 1); // 2로 나누는 연산. i->i/2로 만들기 위해 연산 1번 + i/2->1로 만들기 위해 필요한 연산 d[i/2]
             }
-            if(i % 3 == 0) {
-                d[i] = Math.min(d[i], d[i / 3] + 1);
+            if(i % 3 == 0) { // 3으로 나누어 떨어지는 경우
+                d[i] = Math.min(d[i], d[i / 3] + 1); // 3으로 나누는 연산. i->i/3로 만들기 위한 연산 1번 + i/3->1로 만들기 위해 필요한 연산 d[i/3]
             }
         }
-        System.out.println(d[x]);
+        System.out.println(d[x]); // x를 1로 만드는 데 필요한 최소 연산 횟수 출력
     }
 }

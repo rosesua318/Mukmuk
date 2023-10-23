e4d3bc531c758991bd816695515d4a1f4e11f7b6
rosesua <rosesua@duksung.ac.kr>
2023-10-23 18:57:43 +0900

    인프런 송아지를 잡자, 집을 짓자 코드 설명 추가

--- bfs/inflearn/BuildHouse.java
+++ bfs/inflearn/BuildHouse.java
@@ -8,12 +8,12 @@ public class BuildHouse {
         int dx[] = {-1, 0, 1, 0};
         int dy[] = {0, 1, 0, -1};
         int n = board.length;
-        int dist[][] = new int[n][n]; // 모든 빌딩에서 해당 지점으로 가는 데 최단거리로 가는 이동거리의 총합
+        int dist[][] = new int[n][n]; // dist[i][j] : 모든 빌딩에서 (i,j) 지점으로 가는 최단거리의 합
         Queue<int[]> q = new LinkedList<>();
-        int emptyLand = 0; // 빈땅의 값
-        for(int i = 0; i < n; i++) {
+        int emptyLand = 0; // 빈땅의 값을 나타낸다. 한 빌딩 탐색 끝날 때 마다 -1 감소 시켜주어 구분.
+        for(int i = 0; i < n; i++) { // board 탐색
             for(int j = 0; j < n; j++) {
-                if(board[i][j] == 1) { // 빌딩을 만난 경우 -> 빌딩에서부터 bfs탐색해서 각 빈땅으로 가는 최단 거리 구함
+                if(board[i][j] == 1) { // 빌딩을 만난 경우 -> 빌딩에서부터 bfs탐색해서 해당 빌딩에서 각 빈땅으로 가는 최단 거리를 dist 배열에 기록
                     answer = Integer.MAX_VALUE; // 한 빌딩이라도 막혀있는 경우 -1을 리턴시키기 위해 매 빌딩마다 answer를 MAX로 초기화
                     q.offer(new int[]{i, j});
                     int L = 0;
@@ -25,20 +25,20 @@ public class BuildHouse {
                             for(int k = 0; k < 4; k++) { // 자식 노드 탐색
                                 int nx = cur[0] + dx[k];
                                 int ny = cur[1] + dy[k];
-                                if(nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] == emptyLand) { // 범위 안이면서 빈땅인 경우
-                                    board[nx][ny]--; // 빈땅지점의 값을 1 감소 시킴
+                                if(nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] == emptyLand) { // 범위 안이면서 해당 빌딩 기준 빈땅인 경우
+                                    board[nx][ny]--; // 빈땅지점의 값을 1 감소 시킴 (다음 빌딩이 탐색할 때 구분하기 위함-공통되는 빈땅인지)
                                     dist[nx][ny] += L; // L(이동거리) 누적 해주기
                                     q.offer(new int[]{nx, ny});
-                                    answer = Math.min(answer, dist[nx][ny]); // 최단거리 누적합 업데이트
+                                    answer = Math.min(answer, dist[nx][ny]); // 최단거리의 합 최소값으로 업데이트
                                 }
                             }
                         }
                     }
-                    emptyLand--; // 모든 빌딩들의 교집합인 빈땅여부 확인하기 위한 변수이므로 감소해서 업데이트 시켜줌
+                    emptyLand--; // 모든 빌딩들의 교집합인 빈땅을 구분하기 위한 변수이므로 한 빌딩 탐색할 때 마다 감소해서 업데이트 시켜줌
                 }
             }
         }
-        return answer == Integer.MAX_VALUE ? -1 : answer;
+        return answer == Integer.MAX_VALUE ? -1 : answer; // MAX인 경우 공통되는 빈땅이 없기 때문에 -1 리턴. 아니면 최소값 리턴
     }
 
     public static void main(String[] args) {
--- bfs/inflearn/Calf.java
+++ bfs/inflearn/Calf.java
@@ -3,10 +3,10 @@ package bfs.inflearn;
 import java.util.*;
 
 public class Calf {
-    public static int solution(int s, int e) {
-        int check[][] = new int[2][200001];
+    public static int solution(int s, int e) { // s : 현수 시작 위치, e : 송아지 시작 위치
+        int check[][] = new int[2][200001]; // 0행 : 짝수 레벨(초)에 방문하는 지점 체크, 1행 : 홀수 레벨(초)에 방문하는 지점 체크
         Queue<Integer> q = new LinkedList<>();
-        check[0][s] = 1; // 0초는 짝수레벨이므로 0행에 1표시 (1행은 홀수레벨일때)
+        check[0][s] = 1; // 0초는 짝수레벨이므로 0행에 방문 표시
         q.offer(s);
         int L = 0; // 레벨 표시 (초의 흐름)
         while(!q.isEmpty()) {
@@ -14,19 +14,19 @@ public class Calf {
             L++; // 아래 for문의 자식노드인 nx의 레벨을 나타냄
             for(int i = 0; i < len; i++) {
                 int x = q.poll();
-                for(int nx : new int[]{x - 1, x + 1, x * 2}) {
-                    if(nx >= 0 && nx <= 200000 && check[L % 2][nx] == 0) { // 범위 안이고 체크되어있지 않은 경우
-                        check[L % 2][nx] = 1;
+                for(int nx : new int[]{x - 1, x + 1, x * 2}) { // 다음 레벨 지점들
+                    if(nx >= 0 && nx <= 200000 && check[L % 2][nx] == 0) { // 범위 안이고 홀수 or 짝수 레벨에 방문하지 않은 경우
+                        check[L % 2][nx] = 1; // 홀수 or 짝수 레벨에 방문했음을 표시
                         q.offer(nx);
                     }
                 }
             }
-            e += L; // 송아지가 이동한 L만큼 더해서 위치 e 구함
-            if(e > 200000) { // 범위 밖인 경우
-                return - 1;
+            e += L; // 송아지가 이동한 L만큼 더해서 위치 e 업데이트 해줌
+            if(e > 200000) { // 송아지의 위치가 범위 밖인 경우
+                return - 1; // -1 리턴
             }
             // L초에 현수가 방문할 수 있는 모든 위치는 1로 표시되어있음 -> check배열에 e의 위치가 1로 표시되어있으면 잡은것
-            if(check[L % 2][e] == 1) { // L초에 현수와 송아지가 같은 위치에 있는 것이 가능한 경우
+            if(check[L % 2][e] == 1) { // L초에 현수와 송아지가 같은 위치에 있는 것이 가능한 경우 -> 잡은 것
                 return L; // L초 리턴
             }
         }
